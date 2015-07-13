dab6f15ce12a48ca7bdd8e1e6ba4f349eebe8063
pikesley <github@orgraphone.org>
2015-07-13 17:23:24 +0100

    I think this all works now

---
+++ test/integration/production/serverspec/env_spec.rb
@@ -0,0 +1,8 @@
+require 'serverspec'
+set :backend, :exec
+
+describe file '/home/vagrant/env' do
+  it { should be_file }
+  its(:content) { should match /DEFAULT: overridden/ }
+  its(:content) { should match /JUST_CANNOT: even/ }
+end
--- test/integration/staging/serverspec/env_spec.rb
+++ test/integration/staging/serverspec/env_spec.rb
@@ -3,5 +3,6 @@ set :backend, :exec
 
 describe file '/home/vagrant/env' do
   it { should be_file }
+  its(:content) { should match /DEFAULT: value/ }
   its(:content) { should match /WHAT: even/ }
 end
