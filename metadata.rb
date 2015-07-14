8c5fea7f382c0639057fa2af45f475038c9ac711
pikesley <github@orgraphone.org>
2015-07-14 17:14:01 +0100

    Bump this
    Because this is definitely a breaking change

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'Installs/Configures envbuilder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '1.0.0'
